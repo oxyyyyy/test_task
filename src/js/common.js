216f44265d09abe846847872d1ee06cb8e865732
Alexandr Vlasenko <olexandr.vlasenko@gmail.com>
2018-02-28 17:45:23 +0200

    things todo

--- src/js/common.js
+++ src/js/common.js
@@ -3,6 +3,7 @@ window.onscroll = function() {myFunction()};
 
 // Get the header
 var header = document.getElementById("header__nav--sticky");
+var welcomeSection = document.getElementById("welcome");
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
@@ -10,8 +11,10 @@ var sticky = header.offsetTop;
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky-fixed");
+		header.classList.add("sticky-fixed");
+		welcomeSection.classList.add("plus-pt");
   } else {
-    header.classList.remove("sticky-fixed");
+		header.classList.remove("sticky-fixed");
+		welcomeSection.classList.remove("plus-pt");
   }
 }
