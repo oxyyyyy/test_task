97d9e91f48081a52d50cde8fdcb2e5c75e9a9f47
Alexandr Vlasenko <olexandr.vlasenko@gmail.com>
2018-03-01 16:21:43 +0200

    before WOW.js

--- src/js/common.js
+++ src/js/common.js
@@ -25,3 +25,17 @@ function myFunction() {
 $("#Glide").glide({
 	type: "carousel"
 });
+$("#Glide2").glide({
+	type: "carousel",
+	autoplay: false
+});
+
+
+// --- Smooth scroll
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+	event.preventDefault();
+	$('html, body').animate({
+		scrollTop: $($.attr(this, 'href')).offset().top
+	}, 500);
+});
